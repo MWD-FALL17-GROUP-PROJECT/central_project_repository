ed0f2bbcdc370ac6b27d3407c30fd82948a9e43b
ishanshrivastava2011 <ishan.shrivastava2011@gmail.com>
2017-10-22 21:31:11 -0700

    Adding checks for task3 and task4

--- MWD/tasks/task3.py
+++ MWD/tasks/task3.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from computations import tasksBusiness
+from data import DataHandler
 
 def task3a(seed):
+    DataHandler.createDictionaries1()
+    actor_movie_rank_map = DataHandler.actor_movie_rank_map
+    for s in seed:
+        if s not in actor_movie_rank_map:
+            print('Invalid seed actor id : '+str(s))
+            return
     tasksBusiness.PersnalizedPageRank_top10_SimilarActors(seed)
     
 def task3b(seed):
+    DataHandler.createDictionaries1()
+    actor_movie_rank_map = DataHandler.actor_movie_rank_map
+    for s in seed:
+        if s not in actor_movie_rank_map:
+            print('Invalid seed actor id : '+str(s))
+            return
     tasksBusiness.PersnalizedPageRank_top10_SimilarCoActors(seed)
--- MWD/tasks/task4.py
+++ MWD/tasks/task4.py
@@ -1,5 +1,9 @@
 from computations import tasksBusiness
-
+from data import DataHandler
 def task4(user_id):
-    movies = tasksBusiness.Recommender(user_id)
-    print([k for (k,y) in movies[0:5]])
+    DataHandler.vectors()
+    if (user_id in DataHandler.user_rated_or_tagged_map):
+        movies = tasksBusiness.Recommender(user_id)
+        print([k for (k,y) in movies[0:5]])
+    else:
+        print('Invalid User ID : ' + str(user_id))
