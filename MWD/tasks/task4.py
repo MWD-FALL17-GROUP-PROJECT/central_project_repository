8d86e07cb75dd537883f28365b0a40d756b4e29b
Ramanuj Bhattacharjee <31291707+ramanuj93@users.noreply.github.com>
2017-10-22 20:44:18 -0700

    task4 final changes

--- MWD/tasks/task4.py
+++ MWD/tasks/task4.py
@@ -1,4 +1,5 @@
 from computations import tasksBusiness
 
 def task4(user_id):
-    print(tasksBusiness.Recommender(user_id))
+    movies = tasksBusiness.Recommender(user_id)
+    print([k for (k,y) in movies[0:5]])
